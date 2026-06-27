7fe6cc3fea03fa7403de882b57871b908e577ac2
Tobi <120692959+tobiawolaju@users.noreply.github.com>
2026-06-27 21:52:28 +0100

    :)

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,14 +1,25 @@
-const CACHE = 'rugpool-v1';
+const CACHE = 'rugpool-v2';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(['/']))
-  );
+self.addEventListener('install', () => {
   self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
